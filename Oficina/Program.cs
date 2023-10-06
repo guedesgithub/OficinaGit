62ff8bf820314e1db530cbd04c440331c18c3d0c
Kauan <gabrielguedes.works1313@gmail.com>
2023-10-06 09:38:53 -0400

    segundo commit

--- Oficina/Program.cs
+++ Oficina/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um numero: ");
+        double num = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite outro numero: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double resultado = num * num2;
+
+        if (resultado == 0) { Console.WriteLine("Numero inválido"); }
+        if (resultado == 1) { Console.WriteLine("Numero inválido"); }
+        if (resultado == 2) { Console.WriteLine("Numero inválido"); }
+       /* if (resultado == 3) { Console.WriteLine("Numero inválido"); }*/
+
+
+
+        Console.WriteLine($"O resultado da multiplicação é {resultado}");
+
+        Console.ReadKey(); 
+    }
+}
